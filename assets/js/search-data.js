1ff079f37d1ad85a981e928005051de6f023d5ff
kaseyzapatka <33298844+kaseyzapatka@users.noreply.github.com>
2025-09-05 22:54:05 +0000

    Deploying to gh-pages from @ kaseyzapatka/kaseyzapatka.github.io@59d0e0657e36ddace09ee9e391ffc377193b274a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,7 +46,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
