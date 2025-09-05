f3639e625a0afa57ba71f192c6bd0ca1ccd7acec
kaseyzapatka <33298844+kaseyzapatka@users.noreply.github.com>
2025-09-05 23:21:20 +0000

    Deploying to gh-pages from @ kaseyzapatka/kaseyzapatka.github.io@bc4f831dea556906bc7ab7e677d390357e13ab8f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-i-received-my-phd",
+          title: 'I received my PhD!',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-i-started-my-postdoc-at-university-of-california-berkeley-smile",
+          title: 'I started my Postdoc at University of California, Berkeley! :smile:',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
