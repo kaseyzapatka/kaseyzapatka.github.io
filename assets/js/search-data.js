bb64396a1dab7e1278060666658bf2f1f62d197c
kaseyzapatka <33298844+kaseyzapatka@users.noreply.github.com>
2025-09-06 04:46:41 +0000

    Deploying to gh-pages from @ kaseyzapatka/kaseyzapatka.github.io@e26f46d45654cfcd15d9d155bff2b0e19d1d08b7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
