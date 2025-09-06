5234cb0e1a1f9ee044c70fe86b2e57351387e903
kaseyzapatka <33298844+kaseyzapatka@users.noreply.github.com>
2025-09-06 03:36:06 +0000

    Deploying to gh-pages from @ kaseyzapatka/kaseyzapatka.github.io@640bfcec5158fc5baa237cb6522224fb3c8c5e13 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -503,7 +503,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%6B%61%73%65%79%7A%61%70%61%74%6B%61@%67%6D%61%69%6C%63%6F%6D.%63%6F%6D", "_blank");
+          window.open("mailto:%6B%61%73%65%79%7A%61%70%61%74%6B%61@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-github',
@@ -513,18 +513,11 @@ ninja.data = [{
           window.open("https://github.com/kaseyzapatka", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.linkedin.com/in/www.linkedin.com/in/kaseyzapatka", "_blank");
+          window.open("https://www.linkedin.com/in/kaseyzapatka", "_blank");
         },
       },{
         id: 'social-orcid',
@@ -534,18 +527,11 @@ ninja.data = [{
           window.open("https://orcid.org/0000-0002-7278-3660", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=5nHsgg0AAAAJ", "_blank");
         },
       },{
         id: 'social-x',
@@ -553,13 +539,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://twitter.com/kaseyzap", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
