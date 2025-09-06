1a3766419f2c5e58bd9a3aaf5157fbf6f4c4d599
kaseyzapatka <33298844+kaseyzapatka@users.noreply.github.com>
2025-09-06 03:27:05 +0000

    Deploying to gh-pages from @ kaseyzapatka/kaseyzapatka.github.io@4ebfa2b14c96cf4d5eeff1cc6b5e3a3b1fe9510b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,11 +492,25 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-bluesky',
+        title: 'Bluesky',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://bsky.app/profile/kaseyzapatka.bsky.social", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6B%61%73%65%79%7A%61%70%61%74%6B%61@%67%6D%61%69%6C%63%6F%6D.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/kaseyzapatka", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -504,6 +518,20 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/www.linkedin.com/in/kaseyzapatka", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0002-7278-3660", "_blank");
         },
       },{
         id: 'social-rss',
@@ -518,6 +546,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/kaseyzap", "_blank");
         },
       },{
         id: 'social-custom_social',
