a5a35801d56d8af2c58b2a79c4fc6b88628321fa
kaseyzapatka <33298844+kaseyzapatka@users.noreply.github.com>
2025-09-08 22:49:06 +0000

    Deploying to gh-pages from @ kaseyzapatka/kaseyzapatka.github.io@c73172bf954157552e8f621a157aab86476c4b24 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,13 +51,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
